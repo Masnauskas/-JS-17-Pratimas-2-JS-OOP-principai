e4a8a3dfd13ab7e6ba053a0a8c69729d0449ea69
Tadas <tadasmasnauskas@gmail.com>
2022-09-18 15:55:43 +0300

    Problem with img

---
+++ script.js
@@ -0,0 +1,30 @@
+class Cars {
+  constructor(brand, model, mileage, price, image) {
+    this.brand = brand;
+    this.model = model;
+    this.mileage = mileage;
+    this.price = price;
+    this.image = image;
+    this.createTextAndImage = function () {
+      const box = document.createElement("div");
+      box.className = "box";
+      box.addEventListener("click", () => "The price is: " + this.price);
+      const image = document.createElement("img");
+      image.src = this.image;
+      const text = document.createElement("div");
+      text.textContent = this.brand + " " + this.model;
+      box.append(image, text);
+      document.querySelector("div.mainContainer").append(box);
+    };
+  }
+}
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  let brand = document.getElementById("carBrand").value;
+  let model = document.getElementById("carModel").value;
+  let mileage = document.getElementById("carMileage").value;
+  let price = document.getElementById("carPrice").value;
+  let image = document.getElementById("carPrice").value;
+  const car = new Cars(brand, model, mileage, price, image);
+  car.createTextAndImage();
+});
