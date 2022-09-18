5df2045a42f45cd3966bc538ee7fff6d4479cb92
Tadas <tadasmasnauskas@gmail.com>
2022-09-18 15:59:25 +0300

    Problems fixed

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ class Cars {
     this.createTextAndImage = function () {
       const box = document.createElement("div");
       box.className = "box";
-      box.addEventListener("click", () => "The price is: " + this.price);
+      box.addEventListener("click", () =>
+        alert("The price is: " + this.price + " EUR")
+      );
       const image = document.createElement("img");
       image.src = this.image;
       const text = document.createElement("div");
@@ -24,7 +26,7 @@ document.querySelector("form").addEventListener("submit", (event) => {
   let model = document.getElementById("carModel").value;
   let mileage = document.getElementById("carMileage").value;
   let price = document.getElementById("carPrice").value;
-  let image = document.getElementById("carPrice").value;
+  let image = document.getElementById("carImage").value;
   const car = new Cars(brand, model, mileage, price, image);
   car.createTextAndImage();
 });
